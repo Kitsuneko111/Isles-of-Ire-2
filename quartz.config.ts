7b7d7628068179170d1a2a8d39913345e1ec9769
Kitsuneko <53912018+Kitsuneko111@users.noreply.github.com>
2025-07-26 22:32:48 +0100

    Quartz sync: Jul 26, 2025, 10:32 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -67,7 +67,7 @@ const config: QuartzConfig = {
         keepBackground: false,
       }),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
-      //Plugin.GitHubFlavoredMarkdown(),
+      Plugin.GitHubFlavoredMarkdown(),
       Plugin.HardLineBreaks(),
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
